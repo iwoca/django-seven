e63fb8b99edbd0a78116b709b4f9d25ab3e0157a
romgar <romain.garrigues.cs@gmail.com>
2016-05-02 16:32:28 +0100

    Removing useless seven specific settings file + fixing decorator

--- seven/compat/decorators.py
+++ seven/compat/decorators.py
@@ -1,6 +1,6 @@
 from functools import wraps
 
-from seven import settings as seven_settings
+from django.conf import settings
 
 
 def to_tuple(version):
@@ -11,9 +11,9 @@ def available_on(min_supported_version, max_supported_version):
     def wrapped_parameters(func):
         @wraps(func)
         def inner(*args, **kwargs):
-            if (to_tuple(seven_settings.CURRENT_DJANGO_VERSION) > to_tuple(min_supported_version)) and\
-                    (to_tuple(seven_settings.FUTURE_DJANGO_VERSION) < to_tuple(max_supported_version)):
+            if (to_tuple(settings.CURRENT_DJANGO_VERSION) > to_tuple(min_supported_version)) and\
+                    (to_tuple(settings.FUTURE_DJANGO_VERSION) < to_tuple(max_supported_version)):
                 return func(*args, **kwargs)
-            return Exception('You cannot use {function_name} in your context'.format(function_name=str(func)))
+            raise Exception('You cannot use {function_name} in your context'.format(function_name=str(func)))
         return inner
     return wrapped_parameters
--- seven/settings.py
+++
@@ -1,9 +0,0 @@
-
-from django.conf import settings
-
-
-if not hasattr(settings, 'CURRENT_DJANGO_VERSION'):
-    CURRENT_DJANGO_VERSION = "1.4"
-
-if not hasattr(settings, 'FUTURE_DJANGO_VERSION'):
-    FUTURE_DJANGO_VERSION = "1.9"
