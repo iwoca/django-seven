e1e2404e23485a0930c50c5ac81a2a683974f768
romgar <romain.garrigues.cs@gmail.com>
2016-05-02 15:52:17 +0100

    Adding tests for available_on decorator

--- seven/settings.py
+++ seven/settings.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 
 if not hasattr(settings, 'CURRENT_DJANGO_VERSION'):
-    CURRENT_DJANGO_VERSION = (1, 4)
+    CURRENT_DJANGO_VERSION = "1.4"
 
 if not hasattr(settings, 'FUTURE_DJANGO_VERSION'):
-    CURRENT_DJANGO_VERSION = (1, 9)
+    FUTURE_DJANGO_VERSION = "1.9"
