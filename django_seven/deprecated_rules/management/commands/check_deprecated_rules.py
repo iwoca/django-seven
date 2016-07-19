81ac96b21c95e07209b468955bb1cfc179962fdf
romgar <romain.garrigues.cs@gmail.com>
2016-07-19 23:32:31 +0200

    Plug new regex validation version on old django management command logic

--- django_seven/deprecated_rules/management/commands/check_deprecated_rules.py
+++ django_seven/deprecated_rules/management/commands/check_deprecated_rules.py
@@ -4,8 +4,8 @@ import os
 from django.core.management.base import BaseCommand
 
 from django_seven.deprecated_rules import rules
-from django_seven.deprecated_rules.helpers import aggregated_regex, is_excluded_dir, validate_file, is_excluded_file, \
-    compile_regex
+from django_seven.deprecated_rules.helpers import is_excluded_dir, is_excluded_file, compile_regex
+from django_seven.deprecated_rules.new_helpers import validate_file
 
 
 class Command(BaseCommand):
@@ -18,20 +18,26 @@ class Command(BaseCommand):
             If the command is launched without arguments, I get the diff between actual migrations and the frozen ones
             With --save parameters, we save current migrations in the filesystem in a json file.
         """
-        print('Checking deprecated rules...\n')
-        errors = 0
+        print('Check deprecated rules...\n')
         regex_rules = compile_regex(rules.REGEX_RULES)
-        aggregated_regex_compiled = aggregated_regex(regex_rules)
 
-        for rule in sorted(rules.REGEX_RULES.iteritems(), key=lambda x: x[1]['number']):
-            print('{number}: {message}'.format(number=rule[1]['number'], message=rule[1]['message']))
+        for rule in sorted(rules.REGEX_RULES, key=lambda x: x['number']):
+            print('{number}: {message}'.format(number=rule['number'], message=rule['message']))
+
+        report = {}
+
+        def progress_fn(filename, rule, line, number):
+            print(filename)
+            print("{number}: {message}".format(number=rule['number'], message=rule['message']))
+            print("L{line_number}: {line}".format(line_number=number, line=line))
 
         for dirName, subdirList, fileList in os.walk('.'):
             if not is_excluded_dir(dirName):
                 for fname in [f for f in fileList if not is_excluded_file(f, dirName)]:
-                    errors |= int(validate_file(fname, dirName, aggregated_regex_compiled, regex_rules))
+                    file_report = validate_file(os.path.join(dirName, fname), regex_rules, progress_fn)
+                    report.update(file_report)
 
-        if errors:
+        if len(report) > 0:
             print('\nHave a look at http://django-seven.readthedocs.org/en/develop/deprecated-rules for more informations about these errors.\n')
         else:
             print('\nDeprecated rules respected. Good job \o/\n')
--- django_seven/deprecated_rules/new_helpers.py
+++ django_seven/deprecated_rules/new_helpers.py
@@ -1,9 +1,9 @@
+import os
 import re
 from collections import defaultdict
 
 
-def validating_regex(regex, line):
-    pattern = re.compile(regex)
+def validating_regex(pattern, line):
     return pattern.search(line) is not None
 
 
@@ -12,15 +12,15 @@ def parse_file(filename, regex_rules):
     with open(filename) as f:
         for i, line in enumerate(f):
             for rule in regex_rules:
-                if validating_regex(rule['regex'], line):
-                    yield (rule['name'], line, i+1)
+                if validating_regex(rule['compiled_regex'], line):
+                    yield (rule, line, i+1)
 
 
 def validate_file(filename, regex_rules, parse_progress=None):
 
     report = defaultdict(lambda: defaultdict(list))
-    for rule_name, line, number in parse_file(filename, regex_rules):
+    for rule, line, number in parse_file(filename, regex_rules):
         if parse_progress:
-            parse_progress(filename, rule_name, file)
-        report[rule_name]['lines'].append({'content': line, 'number': number})
+            parse_progress(filename, rule, line, number)
+        report[rule['name']]['lines'].append({'content': line, 'number': number, 'filename': filename})
     return report
