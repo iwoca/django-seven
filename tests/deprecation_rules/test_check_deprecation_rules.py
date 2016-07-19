285dff0df129bef83b90a27d4173436d211a49cc
Romain G <romain@iwoca.co.uk>
2016-07-19 22:53:32 +0200

    Remove command call test

--- tests/deprecation_rules/test_check_deprecation_rules.py
+++
@@ -1,9 +0,0 @@
-from django.core.management import call_command
-from django.test import TestCase
-
-
-class DeprecationRulesTest(TestCase):
-
-    def test_check_deprecation_rules_command(self):
-        # call_command('check_deprecated_rules')
-        pass
